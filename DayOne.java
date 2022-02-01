29bcaed4cee22b6e05941ed7cfc480c658b28669
pradattapatil <pradattapatil@gmail.com>
2022-02-02 02:10:30 +0530

    DayOne Added

---
+++ DayOne.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.dayone;
+
+import java.util.Scanner;
+import com.bridgelabz.Helloworld;
+
+public class DayOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Enter two numbers...");
+        Scanner sc = new Scanner(System.in);
+// int i1 = sc.nextInt();
+        System.out.println("Your First input : " + sc.nextInt());
+// int i2 = sc.nextInt();
+        System.out.println("Your Second input : " + sc.nextInt());
+// Helloworld.multiply(i1, i2);
+    }
+}
